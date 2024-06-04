7d56779ce13c8c2a2f96388af8634af76c0a9c2a
Tiago França <tiago@tiagofranca.com>
2024-06-03 21:00:11 -0300

    customer email show
    On branch task/customer-crud
    	modified:   resources/views/customers/partials/update-profile-information-form.blade.php

--- resources/views/customers/partials/update-profile-information-form.blade.php
+++ resources/views/customers/partials/update-profile-information-form.blade.php
@@ -47,6 +47,31 @@
                 </div>
             @endif
         </div>
+        @else
+        <div>
+            <x-input-label :value="__('Email')" />
+            <div
+                @class([
+                    'border',
+                    'bg-gray-400/50',
+                    'border-gray-500',
+                    'dark:border-gray-600',
+                    'dark:bg-gray-600/50',
+                    'dark:text-gray-300',
+                    'focus:border-indigo-500',
+                    'dark:focus:border-indigo-600',
+                    'focus:ring-indigo-500',
+                    'dark:focus:ring-indigo-600',
+                    'rounded-md',
+                    'shadow-sm',
+                    'mt-1',
+                    'block',
+                    'w-full',
+                    'px-3',
+                    'py-2',
+                ])
+            >{{ $customer->email ?? null }}</div>
+        </div>
         @endif
 
         <div class="flex items-center gap-4">
