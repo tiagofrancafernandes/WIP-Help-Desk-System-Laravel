e4034b28ef740696ea67edf0d43556dc5c702711
Tiago França <tiago@tiagofranca.com>
2024-06-20 09:37:38 -0300

    fix update form and rebuild asstes
    On branch task/customer-crud
    	new file:   public/build/assets/app-CKbZv4HQ.css
    	deleted:    public/build/assets/app-Dxpaxzej.css
    	modified:   public/build/manifest.json
    	modified:   resources/views/components/primary-button.blade.php
    	modified:   resources/views/customers/partials/update-password-form.blade.php

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,21 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-0 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
+@props([
+    'disabled' => false,
+])
+
+@php
+$disabledClass = ($disabled ?? null) ? [
+    'bg-gray-600 dark:bg-gray-600',
+] : [
+    'bg-gray-800 dark:bg-gray-200',
+    'hover:bg-gray-700 dark:hover:bg-white',
+    'focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300',
+];
+@endphp
+
+<button
+    {{ $attributes->merge([
+        'type' => 'submit',
+        'class' => 'inline-flex items-center px-4 py-2 ' . implode(' ', $disabledClass) . ' border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest  focus:outline-none focus:ring-0 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150'
+    ]) }}>
     {{ $slot }}
 </button>
--- resources/views/customers/partials/update-password-form.blade.php
+++ resources/views/customers/partials/update-password-form.blade.php
@@ -9,24 +9,65 @@
         </p>
     </header>
 
-    <form method="post" action="{{ route('customers.users.update_password', $customer) }}" class="mt-6 space-y-6">
+    <form
+        method="post" action="{{ route('customers.user.update_password', $customer) }}"
+        class="mt-6 space-y-6"
+        x-data="{
+            password: '',
+            password_confirmation: '',
+            get newPassIsValid() {
+                if (!this.password || this.password?.length < 8) {
+                    return false;
+                }
+
+                console.log('this.password', this.password);
+                return this.password === this.password_confirmation;
+            }
+        }"
+    >
         @csrf
         @method('put')
 
         <div>
             <x-input-label for="update_password_password" :value="__('New Password')" />
-            <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+            <x-text-input
+            id="update_password_password"
+            x-model="password"
+            name="password"
+            type="password"
+            class="mt-1 block w-full" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
         </div>
 
         <div>
-            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+            <x-input-label for="update_password_password_confirmation">
+                {{ __('Confirm Password') }}
+                <span
+                    class="text-md text-red-500 dark:text-red-600"
+                    x-show="password"
+                >*</span>
+            </x-input-label>
+            <x-text-input
+                id="update_password_password_confirmation"
+                x-model="password_confirmation"
+                name="password_confirmation"
+                type="password"
+                class="mt-1 block w-full"
+                autocomplete="new-password"
+                x-bind:required="password"
+            />
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button
+                x-bind:class="{
+                    'bg-gray-600 dark:bg-gray-600': !newPassIsValid,
+                    'bg-gray-800 dark:bg-gray-200': newPassIsValid,
+                    'hover:bg-gray-700 dark:hover:bg-white': newPassIsValid,
+                    'focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300': newPassIsValid,
+                }"
+            >{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'password-updated')
                 <p
