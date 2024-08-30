02779ca05db2a6d7a29a1e8f87af82b48208e86e
Tiago França <tiago@tiagofranca.com>
2024-08-29 23:01:47 -0300

    added icons
    On branch task/favicons
    	new file:   public/favicon-150x150.png
    	new file:   public/favicon-235x235.png
    	new file:   public/favicon-32x32.png
    	modified:   public/favicon.ico
    	new file:   public/favicon.png
    	new file:   public/logo.png
    	modified:   resources/views/components/application-logo.blade.php
    	new file:   resources/views/components/favicons.blade.php
    	modified:   resources/views/layouts/app.blade.php
    	modified:   resources/views/layouts/guest.blade.php
    	modified:   resources/views/welcome.blade.php

---
+++ resources/views/components/favicons.blade.php
@@ -0,0 +1,7 @@
+
+<link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/png">
+<link rel="icon" href="{{ asset('favicon-32x32.png') }}" sizes="32x32"  type="image/png"/>
+<link rel="icon" href="{{ asset('favicon-150x150.png') }}" sizes="150x150"  type="image/png"/>
+<link rel="icon" href="{{ asset('favicon-favicon-235x235.png.png') }}" sizes="favicon-235x235.png"  type="image/png"/>
+<link rel="apple-touch-icon" href="{{ asset('favicon.png') }}"  type="image/png"/>
+<meta name="msapplication-TileImage" content="{{ asset('favicon.png') }}"  type="image/png"/>
